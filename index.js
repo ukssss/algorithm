8efb0a80fe69b72eaf59226edb4acdd99c505449
ukssss <1872003115su@gmail.com>
2022-12-11 13:48:47 +0900

    폴더 구조 및 파일명 변경 (3)

--- index.js
+++ index.js
@@ -4,10 +4,10 @@
 const input = require("fs").readFileSync("example.txt").toString().trim();
 
 // 2. 공백으로 구분된 한 줄의 값들을 입력받을 때
-// const input = require('fs').readFileSync('example.txt').toString().trim().split(' ');
+//const input = require("fs").readFileSync("example.txt").toString().trim().split(" ");
 
 // 3. 여러 줄의 값들을 입력받을 때
-// const input = require('fs').readFileSync('example.txt').toString().trim().split('\n');
+// const input = require("fs").readFileSync("example.txt").toString().trim().split("\n");
 
 // 4. 첫 번째 줄에 자연수 n을 입력받고, 그 다음줄에 공백으로 구분된 n개의 값들을 입력받을 때
 // const [n, ...arr] = require('fs').readFileSync('example.txt').toString().trim().split(/\s/);
@@ -25,4 +25,9 @@ const input = require("fs").readFileSync("example.txt").toString().trim();
 // 2~6에서 입력받는 값들을 모두 String에서 Number로 바꾸려면 split()뒤에 .map(Number)를 추가
 
 // 코딩 스타트 !
-console.log("Hello World!");
+const n = parseInt(input);
+let pizzaBox = 6;
+while (pizzaBox % n !== 0) {
+  pizzaBox += 6;
+}
+console.log(pizzaBox / n);
--- tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-//let input = require("fs").readFileSync("/dev/stdin").toString().split(" ");
-let a = parseInt(input[0]);
-let b = parseInt(input[1]);
-
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log(a / b);
-console.log(a % b);
