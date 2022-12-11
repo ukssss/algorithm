1f2378d8414b1390455e5b8f9cc5e37521d11641
ukssss <1872003115su@gmail.com>
2022-12-11 11:41:35 +0900

    백준 node.js 환경구축

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-// 실습 기본 환경 세팅 코드
-// 한 줄에 하나씩 값이 들어올 때
-// let input = require("fs").readFileSync("example.txt").toString().split('\n');
+// CommonJS Modules
+// 제출할 때는 example.txt -> /dev/stdin 로 변경할 것!
+// 1. 하나의 값을 입력받을 때
+const input = require("fs").readFileSync("example.txt").toString().trim();
 
-// 한 줄에 공백으로 값이 들어올 때
-let input = require("fs").readFileSync("example.txt").toString().split(" ");
+// 2. 공백으로 구분된 한 줄의 값들을 입력받을 때
+// const input = require('fs').readFileSync('example.txt').toString().trim().split(' ');
 
-// 제출용 코드
-//let input = require("fs").readFileSync("/dev/stdin").toString().split(" ");
-let a = parseInt(input[0]);
-let b = parseInt(input[1]);
+// 3. 여러 줄의 값들을 입력받을 때
+// const input = require('fs').readFileSync('example.txt').toString().trim().split('\n');
 
-console.log(a + b);
-console.log(a - b);
-console.log(a * b);
-console.log(parseInt(a / b));
-console.log(a % b);
+// 4. 첫 번째 줄에 자연수 n을 입력받고, 그 다음줄에 공백으로 구분된 n개의 값들을 입력받을 때
+// const [n, ...arr] = require('fs').readFileSync('example.txt').toString().trim().split(/\s/);
+
+// 5. 첫 번째 줄에 자연수 n을 입력받고, 그 다음줄부터 n개의 줄에 걸쳐 한 줄에 하나의 값을 입력받을 때
+// const [n, ...arr] = require('fs').readFileSync('example.txt').toString().trim().split('\n');
+
+// 6. 하나의 값 또는 공백으로 구분된 여러 값들을 여러 줄에 걸쳐 뒤죽박죽 섞여서 입력받을 때
+// ex) n 입력 - 공백으로 구분된 n개의 값 입력 - m 입력 - 여러 줄에 걸쳐 m개의 값 입력
+// const input = require('fs').readFileSync('example.txt').toString().trim().split(/\s/);
+// const n = input[0];
+// const n_arr = input.slice(1, n+1);
+// const [m, ...m_arr] = input.slice(n+1);
+
+// 2~6에서 입력받는 값들을 모두 String에서 Number로 바꾸려면 split()뒤에 .map(Number)를 추가
+
+// 코딩 스타트 !
+console.log("Hello World!");
