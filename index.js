19dee0bcb855d5a240a26d1f9471d6f558563a60
ukssss <1872003115su@gmail.com>
2022-12-11 10:09:11 +0900

    Code Runner 환경설정 (2)

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-let input = require("fs").readFileSync("example.txt").toString().split("\n");
+// 실습 기본 환경 세팅 코드
+// 한 줄에 하나씩 값이 들어올 때
+// let input = require("fs").readFileSync("example.txt").toString().split('\n');
+
+// 한 줄에 공백으로 값이 들어올 때
+let input = require("fs").readFileSync("example.txt").toString().split(" ");
 let a = parseInt(input[0]);
 let b = parseInt(input[1]);
