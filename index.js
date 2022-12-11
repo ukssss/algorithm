956a6004091300ceb1f826b2036b43826f0df1a4
ukssss <1872003115su@gmail.com>
2022-12-11 11:21:04 +0900

    Github Algorithm 초기화

--- index.js
+++ index.js
@@ -4,5 +4,14 @@
 
 // 한 줄에 공백으로 값이 들어올 때
 let input = require("fs").readFileSync("example.txt").toString().split(" ");
+
+// 제출용 코드
+//let input = require("fs").readFileSync("/dev/stdin").toString().split(" ");
 let a = parseInt(input[0]);
 let b = parseInt(input[1]);
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(parseInt(a / b));
+console.log(a % b);
