15ad482c54bdb177fd02730b9b9110216c8488eb
ukss <1872003115su@gmail.com>
2023-01-01 23:23:02 +0900

    [Silver V] Title: 셀프 넘버, Time: 172 ms, Memory: 11192 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.js
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.js
@@ -1,31 +1,32 @@
-function notSelfNumber(n) {
-  let sum = n; // * ex) 123
+function notSelf(n) {
+  let sum = n; // * e.g. n = 123
 
   while (true) {
-    if (n === 0) break; // * n = 0 break
-    sum += n % 10; // * 1(3), 2(2), 3(1)
-    n = parseInt(n / 10); // * 1(12), 2(1), 3(0)
+    if (n === 0) break; // * n = 0 => break;
+    sum += n % 10; // * sum = 123 + 3 + 2 + 1 = 129
+    n = parseInt(n / 10); // * n = 123 => 12 => 1 => 0
   }
 
-  return sum; // * 123 + 3 + 2 + 1 = 129
+  return sum;
 }
 
-function selfNumber(n) {
-  let notSelf = []; // * notSelf
-  let self = []; // * Self
+function self(n) {
+  let notSelfList = []; // * notSelfNum Add
+  let selfList = []; // * selfNum Add
 
-  for (let i = 1; i < n + 1; i++) {
-    let index = notSelfNumber(i);
+  for (let i = 1; i <= n; i++) {
+    let index = notSelf(i);
 
-    if (index < n + 1) {
-      notSelf[index] = true;
+    if (index <= n) {
+      notSelfList[index] = true; 
     }
   }
 
-  for (let i = 1; i < n + 1; i++) {
-    if (!notSelf[i]) self.push(i);
+  for (let i = 1; i <= n; i++) {
+    if (!notSelfList[i]) selfList.push(i); // * false 인 경우 selfList 추가
   }
-  console.log(self.join("\n"));
+
+  console.log(selfList.join("\n")); // * selfList 한 줄에 하나씩 출력
 }
 
-selfNumber(10000);
+self(10000); // * 10000 보다 작거나 같은 셀프 넘버 출력
