b1d5e7a5fe1b38ecfcb55e31ee1918de7e9160e7
ukss <1872003115su@gmail.com>
2023-01-05 23:50:05 +0900

    [Bronze II] Title: 상수, Time: 116 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.js
+++ 백준/Bronze/2908. 상수/상수.js
@@ -1,14 +1,3 @@
-const [a, b] = require("fs").readFileSync("/dev/stdin").toString().trim().split(" ");
-
-const aArr = a.split("");
-const bArr = b.split("");
-
-let aNew = "";
-let bNew = "";
-
-for (let i = aArr.length - 1; i >= 0; i--) {
-  aNew += aArr[i];
-  bNew += bArr[i];
-}
-
-console.log(Number(aNew) > Number(bNew) ? Number(aNew) : Number(bNew));
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split(" ");
+const [a, b] = input.map((v) => [...v].reverse().join(""));
+console.log(a > b ? a : b);
