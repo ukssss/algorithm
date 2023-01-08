6634d84e3909c553749be12ff617f87711759810
ukss <1872003115su@gmail.com>
2023-01-08 22:08:53 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 120 ms, Memory: 9364 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.js
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.js
@@ -1,11 +1,9 @@
-let input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
-
-let test = input[0];
+let input = require("fs").readFileSync("/dev/stdin").toString().trim();
 
 const croatian = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="];
 
 for (let alphabet of croatian) {
-  test = test.split(alphabet).join("a");
+  input = input.split(alphabet).join("a");
 }
 
-console.log(test.length);
+console.log(input.length);
