f8c66660c43a766d1be0cca97c4c332b8987a0d0
ukss <1872003115su@gmail.com>
2023-01-22 15:11:02 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 132 ms, Memory: 10052 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.js
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.js
@@ -1,25 +1,27 @@
 const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n").map(Number);
 
-const test = input.shift();
+const t = input.shift();
 
-for (let i = 0; i < test; i++) {
+for (let i = 0; i < t; i++) {
   const k = input.shift();
   const n = input.shift();
 
-  const room = Array.from(Array(k + 1), () => Array(n + 1).fill(0));
-  // 길이 k + 1 배열 생성에 내용이 모두 0이면서 길이가 n + 1 로 된 배열을 삽입
+  // 길이가 k + 1 인 배열을 생성 후, 배열안에 0 으로 꽉채운 길이가 n + 1 인 배열 생성
+  const apt = Array.from(Array(k + 1), () => Array(n + 1).fill(0));
 
-  // 0 층 계산
+  // 0 층
   for (let j = 1; j <= n; j++) {
-    room[0][j] = j;
+    apt[0][j] = j;
   }
 
-  // 1 층 ~ k 층 계산
+  // 1 층 ~ k 층
   for (let f = 1; f <= k; f++) {
-    for (let h = 1; h <= n; h++) {
-      room[f][h] = room[f - 1][h] + room[f][h - 1];
+    for (let j = 1; j <= n; j++) {
+      // f 층 j 호 입주민의 수는 f - 1 층의 j 호의 입주민 수에 f 층의 j - 1 호 입주민 수를 더한 것과 같다.
+      apt[f][j] = apt[f - 1][j] + apt[f][j - 1];
     }
   }
 
-  console.log(room[k][n]);
+  // k 층 n 호에 사는 입주민의 수 출력
+  console.log(apt[k][n]);
 }
