4b5114fd1a07bbe302b7ac1c9d617676068ebc2f
ukss <1872003115su@gmail.com>
2023-01-23 02:19:20 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,12 +1,13 @@
 function solution(emergency) {
-    let sorted = [...emergency].sort((a, b) => b - a);
+    // 내림차순 정렬, sort 는 원배열이 정렬되기에 스프레드 연산자를 사용해서 진행
+    const sortDown = [...emergency].sort((a, b) => b - a);
     let answer = [];
-    for (let i = 0; i < emergency.length; i++) {
-        for (let j = 0; j < sorted.length; j++) {
-            if (emergency[i] === sorted[j]) {
-                answer.push(j+1);
-            }
-        }
+
+    // for of 사용
+    for (const element of emergency) {
+        // sortDown, emergency 비교해서 인덱스 값 추출, 0 부터 시작하기에 1 증가
+        answer.push(sortDown.indexOf(element) + 1);
     }
+
     return answer;
 }
