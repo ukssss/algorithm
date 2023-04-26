a2570e694a7deb78a78a0b37e7145976531c18bf
ukss <1872003115su@gmail.com>
2023-04-26 17:31:58 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 132 ms, Memory: 9444 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.js
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.js
@@ -1,19 +1,16 @@
 const [n, ...arr] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
-let [N, M] = n.split(' ').map(Number);
-const basket = Array.from({ length: N }, (value, i) => (value = i + 1));
+let [N, M] = n.toString().trim().split(' ').map(Number);
+let basket = Array.from({ length: N }, (value, i) => (value = i + 1));
 
-for (let k = 0; k < M; k++) {
-  let [i, j] = arr[k].split(' ').map(Number);
-  let cnt = 1;
-
-  for (let e = i - cnt; e <= j - cnt; e++) {
-    let temp1 = basket[j - cnt];
-    let temp2 = basket[e];
-    basket[j - cnt] = temp2;
-    basket[e] = temp1;
-    cnt++;
+for (let a = 0; a < M; a++) {
+  let [i, j] = arr[a].split(' ').map(Number);
+  let temp = [];
+  for (let b = i - 1; b < j; b++) {
+    temp.push(basket[b]);
   }
+  temp.reverse();
+  basket.splice(i - 1, j - i + 1, ...temp);
 }
 
 console.log(basket.join(' '));
