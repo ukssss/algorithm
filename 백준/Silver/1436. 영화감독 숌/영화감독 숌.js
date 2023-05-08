f10b0e9facc7ed106bc4ce9624158f9ebe05ce34
ukssss <1872003115su@gmail.com>
2023-05-08 23:08:10 +0900

    [Silver V] Title: 영화감독 숌, Time: 432 ms, Memory: 23240 KB -BaekjoonHub (풀이)

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.js
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.js
@@ -1,14 +1,12 @@
 let input = Number(require('fs').readFileSync('/dev/stdin').toString().trim());
 
 let titleNum = 665;
-let ans = 0;
 
 while (input > 0) {
   titleNum++;
   if (titleNum.toString().includes('666')) {
-    ans = titleNum;
     input--;
   }
 }
 
-console.log(ans);
+console.log(titleNum);
