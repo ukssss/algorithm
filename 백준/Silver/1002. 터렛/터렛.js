67b5a5f4614a5e3bc3a46f1235d5d058cbe9bee8
ukss <1872003115su@gmail.com>
2023-06-21 23:28:52 +0900

    [Silver III] Title: 터렛, Time: 116 ms, Memory: 9400 KB -BaekjoonHub

--- 백준/Silver/1002. 터렛/터렛.js
+++ 백준/Silver/1002. 터렛/터렛.js
@@ -1,6 +1,8 @@
-const fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-let input = fs.readFileSync(filePath).toString().trim().split('\n');
+const input = require('fs')
+    .readFileSync('dev/stdin', 'utf-8')
+    .toString()
+    .trim()
+    .split('\n');
 let answer = '';
 let T = Number(input.shift());
 
