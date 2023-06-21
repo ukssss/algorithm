8a6a50c27841dec27003f23fa46c556dbe8fc9a2
ukss <1872003115su@gmail.com>
2023-06-21 23:30:55 +0900

    [Silver III] Title: 터렛, Time: 116 ms, Memory: 9380 KB -BaekjoonHub

--- 백준/Silver/1002. 터렛/터렛.js
+++ 백준/Silver/1002. 터렛/터렛.js
@@ -3,12 +3,12 @@ const input = require('fs')
     .toString()
     .trim()
     .split('\n');
-let answer = '';
+let answer = [];
 let T = Number(input.shift());
 
 for(let i=0; i < T; i++) {
-    let arr = input[i].split(' ').map(x=>+x);
-    // console.log(arr);
+    let arr = input[i].split(' ').map(Number);
+
     let x1, x2, y1, y2, r1, r2;
     if(arr[2] > arr[5]) {
         r1 = arr[5];
@@ -28,23 +28,24 @@ for(let i=0; i < T; i++) {
     let sum = r1 + r2;
     let sub = r2 - r1;
     let len = Math.sqrt((x1-x2)**2 + (y1-y2)**2);
-    // console.log(sum, len);
+
     if(sub < len && sum > len) {
-        answer += '2' + '\n';
+        answer.push(2);
     } else if(sum === len) {
-        answer += '1' + '\n';
+        answer.push(1);
     } else if(sub === len && len !==0) {
-        answer += '1' + '\n';
+        answer.push(1);
     } else if(sum < len) {
-        answer += '0' + '\n';
+        answer.push(0);
     } else if(sub > len) {
-        answer += '0' + '\n';
+        answer.push(0);
     } else if(len === 0) {
         if(r1 === r2) {
-            answer += '-1' + '\n';
+            answer.push(-1);
         } else {
-            answer += '0' + '\n';
+            answer.push(0);
         }
     }
 }
-console.log(answer);
+
+console.log(answer.join('\n'));
