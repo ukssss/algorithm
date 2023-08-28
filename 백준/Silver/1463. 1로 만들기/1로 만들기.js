3d644a7b6524e9cd2d05ef6cf780b2af55ff483c
ukss <1872003115su@gmail.com>
2023-08-28 14:43:47 +0900

    [Silver III] Title: 1로 만들기, Time: 196 ms, Memory: 17444 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.js
@@ -0,0 +1,22 @@
+const input = +require('fs').readFileSync('/dev/stdin').toString().trim();
+
+function solution(n) {
+    const dp = new Array(n + 1).fill(0);
+
+    for (let i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + 1;
+
+        if (i % 2 === 0) {
+            dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+        }
+
+        if (i % 3 === 0) {
+            dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+        }
+    }
+
+    return dp[n];
+}
+
+const ans = solution(input);
+console.log(ans);
