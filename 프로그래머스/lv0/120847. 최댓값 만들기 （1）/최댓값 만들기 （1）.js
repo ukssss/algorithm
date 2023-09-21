0b3d9178f7424cd035f79ada7b2ccb35da7da5f4
ukss <1872003115su@gmail.com>
2023-09-21 23:47:22 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -1,12 +1,4 @@
 function solution(numbers) {
-    let max = 0;
-    
-    for(let i = 0; i < numbers.length; i++) {
-        for(let j = 0; j < i; j++) {
-            let temp = numbers[i] * numbers[j];
-            max = Math.max(max, temp);
-        } 
-    }
-    
-    return max;
+    numbers.sort((a,b) => b-a);
+    return numbers[0] * numbers[1];
 }
