6ba49b44b5ea3af5576417207015da5f4ce4b189
ukss <1872003115su@gmail.com>
2023-10-18 14:10:59 +0900

    [level 2] Title: 의상, Time: 3.26 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -0,0 +1,21 @@
+function solution(clothes) {
+    const clothesMap = {};
+    let answer = 1;
+
+    clothes.forEach((arr) => {
+        const [type, name] = arr;
+        if (clothesMap.hasOwnProperty(name)) {
+            clothesMap[name]++;
+        } else {
+            clothesMap[name] = 1;
+        }
+    });
+
+    console.log(clothesMap);
+
+    for (const key in clothesMap) {
+        answer *= clothesMap[key] + 1;
+    }
+
+    return answer - 1;
+}
