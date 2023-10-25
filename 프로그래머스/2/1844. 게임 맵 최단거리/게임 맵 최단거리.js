2e69d5d5553b72df682df45aa5817e1f00c17ebd
ukss <1872003115su@gmail.com>
2023-10-25 23:40:07 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 29.58 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,19 +1,73 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor(data) {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    enqueue(data) {
+        const newNode = new Node(data);
+
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+
+        this.size++;
+    }
+
+    dequeue() {
+        if (!this.head) {
+            return null;
+        }
+
+        const removeNode = this.head;
+        this.head = this.head.next;
+        if (!this.head) {
+            this.tail = null;
+        }
+
+        this.size--;
+
+        return removeNode.data;
+    }
+
+    size() {
+        return this.size;
+    }
+
+    isEmpty() {
+        return this.size === 0;
+    }
+}
+
 function solution(maps) {
     const n = maps.length;
     const m = maps[0].length;
     const dx = [0, 0, -1, 1];
     const dy = [-1, 1, 0, 0];
-    const queue = [[0, 0]];
+    const queue = new Queue();
+    queue.enqueue([0, 0]);
     const visited = Array.from({ length: n }, () => Array.from({ length: m }, () => false));
     visited[0][0] = true;
     let cnt = 0;
 
-    while (queue.length) {
-        const size = queue.length;
+    while (queue.size > 0) {
+        const size = queue.size;
         cnt++;
 
         for (let i = 0; i < size; i++) {
-            const node = queue.shift();
+            const node = queue.dequeue();
 
             for (let j = 0; j < 4; j++) {
                 const nx = node[0] + dx[j];
@@ -33,7 +87,7 @@ function solution(maps) {
                     }
 
                     visited[nx][ny] = true;
-                    queue.push([nx, ny]);
+                    queue.enqueue([nx, ny]);
                 }
             }
         }
