21edfbafb88aa44bf723d6c74de62e2e07d968d7
ukss <1872003115su@gmail.com>
2023-10-28 22:14:17 +0900

    [level 2] Title: 배달, Time: 0.68 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.js
+++ 프로그래머스/2/12978. 배달/배달.js
@@ -1,34 +1,31 @@
 function solution(N, road, K) {
-    const graph = Array.from({length: N + 1}, () => []);
-    
-    for(let info of road) {
-        const [a,b,c] = info;
+    const graph = Array.from({ length: N + 1 }, () => []);
+
+    for (let [a, b, c] of road) {
         graph[a].push({ to: b, dist: c });
-        graph[b].push({ to: a, dist: c});
+        graph[b].push({ to: a, dist: c });
     }
-    
-    const dist = Array(graph.length).fill(Infinity);
+
+    const dist = Array.from({ length: graph.length }, () => Infinity);
     const queue = [{ to: 1, dist: 0 }];
     dist[1] = 0;
-    
-    while(queue.length) {
-        const { to } = queue.pop();
-        
+
+    while (queue.length) {
+        const { to } = queue.shift();
+
         graph[to].forEach((next) => {
             const acc = dist[to] + next.dist;
-            if(dist[next.to] > acc) {
+            if (dist[next.to] > acc) {
                 dist[next.to] = acc;
                 queue.push(next);
             }
-        })
+        });
     }
-    
+
     let cnt = 0;
-    for(let element of dist) {
-        if(element <= K) cnt++;
-    }
-    
-    console.log(dist);
-    
+    dist.forEach((distance) => {
+        distance <= K && cnt++;
+    });
+
     return cnt;
 }
