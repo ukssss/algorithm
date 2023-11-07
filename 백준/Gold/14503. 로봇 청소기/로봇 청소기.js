9a278bff9ecfb2f92caa394c12c3776736c6a179
ukss <1872003115su@gmail.com>
2023-11-07 16:46:52 +0900

    [Gold V] Title: 로봇 청소기, Time: 128 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.js
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.js
@@ -1,41 +1,40 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 const [N, M] = input.shift().split(' ').map(Number);
-let [r, c, d] = input.shift().split(' ').map(Number);
+let [row, col, dir] = input.shift().split(' ').map(Number);
 
 const room = input.map((arr) => arr.split(' ').map(Number));
 const visited = Array.from({ length: N }, () => Array.from({ length: M }, () => false));
 const drow = [-1, 0, 1, 0];
 const dcol = [0, 1, 0, -1];
-
-let ans = 0;
 let cnt = 0;
+let ans = 0;
 
 while (1) {
     if (cnt === 4) {
-        const [backRow, backCol] = [r + drow[(d + 6) % 4], c + dcol[(d + 6) % 4]];
+        const [backRow, backCol] = [row + drow[(dir + 2) % 4], col + dcol[(dir + 2) % 4]];
         if (room[backRow][backCol] === 1) break;
         else {
-            r = backRow;
-            c = backCol;
+            row = backRow;
+            col = backCol;
             cnt = 0;
         }
     }
 
-    if (!visited[r][c]) {
-        visited[r][c] = true;
-        room[r][c] = 2;
+    if (!visited[row][col]) {
+        visited[row][col] = true;
+        room[row][col] = 2;
         ans++;
     }
 
-    const [leftRow, leftCol] = [r + drow[(d + 3) % 4], c + dcol[(d + 3) % 4]];
+    const [leftRow, leftCol] = [row + drow[(dir + 3) % 4], col + dcol[(dir + 3) % 4]];
     if (room[leftRow][leftCol] === 0) {
-        r = leftRow;
-        c = leftCol;
+        row = leftRow;
+        col = leftCol;
         cnt = 0;
     } else {
         cnt++;
     }
-    d = (d + 3) % 4;
+    dir = (dir + 3) % 4;
 }
 
 console.log(ans);
