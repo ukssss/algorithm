dd29e866fa8edd47adcef50d4d27bd4c0f439763
ukss <1872003115su@gmail.com>
2023-11-14 23:20:44 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 8.60 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.js
@@ -1,3 +1,56 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    enqueue(data) {
+        const newNode = new Node(data);
+
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+
+        this.size++;
+    }
+
+    dequeue() {
+        if (!this.head) {
+            return null;
+        }
+
+        const removeNode = this.head;
+        this.head = this.head.next;
+
+        if (!this.head) {
+            this.tail = null;
+        }
+
+        this.size--;
+        return removeNode.data;
+    }
+
+    isEmpty() {
+        return this.size === 0;
+    }
+
+    getSize() {
+        return this.size;
+    }
+}
+
 function solution(n, wires) {
     let min = Infinity;
 
@@ -14,19 +67,22 @@ function solution(n, wires) {
             }
         });
 
-        let stack = [1];
-        const visited = [];
+        const queue = new Queue();
+        const visited = new Set();
+        queue.enqueue(1);
 
-        while (stack.length) {
-            const node = stack.shift();
-            if (!visited.includes(node)) {
-                stack = [...stack, ...connect[node]];
-                visited.push(node);
+        while (!queue.isEmpty()) {
+            const node = queue.dequeue();
+            if (!visited.has(node)) {
+                connect[node].forEach((point) => {
+                    queue.enqueue(point);
+                });
+                visited.add(node);
             }
         }
 
-        let first = n - visited.length;
-        let second = visited.length;
+        let first = n - visited.size;
+        let second = visited.size;
         min = Math.min(min, Math.abs(first - second));
     });
 
