213c967404a40dc6890a06de45f23d4d1d78b62d
ukss <1872003115su@gmail.com>
2023-12-14 20:14:11 +0900

    [level 1] Title: 문자열 나누기, Time: 0.56 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
@@ -1,25 +1,19 @@
 function solution(s) {
-    const arr = s.split("");
-    const ans = [];
-
-    while(arr.length > 0) {
-        let cnt = 0;
-        const node = arr[cnt];
-        let first = [node];
-        let second = [];
-        cnt++
-        
-        while(true) {
-            const temp = arr[cnt];
-            if(node === temp) first.push(temp);
-            else second.push(temp);
-            
-            if(first.length !== second.length) cnt++;
-            else break;
-        }
+    let ans = 0;
+    let count = 0;
+    let current = 0;
     
-        ans.push(arr.splice(0, cnt + 1).join(""));
+    for(let i = 0; i < s.length; i++) {
+        if(count === 0) {
+            ans++;
+            current = s[i];
+            count++;
+        } else if (current !== s[i]) {
+            count--;
+        } else if (current === s[i]) {
+            count++;
+        }
     }
     
-    return ans.length;
+    return ans;
 }
