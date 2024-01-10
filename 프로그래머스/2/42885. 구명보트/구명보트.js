8b0a9054d96f791054b5ff6573e7c6d8a82fc827
ukss <1872003115su@gmail.com>
2024-01-10 17:57:07 +0900

    [level 2] Title: 구명보트, Time: 11.53 ms, Memory: 38 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -0,0 +1,17 @@
+function solution(people, limit) {
+    people.sort((a, b) => b - a);
+    let l = 0;
+    let r = people.length - 1;
+    let cnt = 0;
+    
+    while (l < r) {
+        const sum = people[l] + people[r];
+        
+        if(sum <= limit) r--;
+        l++;
+        cnt++;
+    }
+    
+    if (l === r) cnt++;
+    return cnt;
+}
