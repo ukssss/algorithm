918b697de34bfb3b47e466934333e0bccafce53f
ukss <1872003115su@gmail.com>
2024-01-16 19:50:23 +0900

    [level Lv.2] Title: 피로도, Time: 0.18 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/Lv.2/87946. 피로도/피로도.js
+++ 프로그래머스/Lv.2/87946. 피로도/피로도.js
@@ -1,20 +1,24 @@
-function dfs(k, dungeons, visited, result, cnt) {
-    for (let i = 0; i < dungeons.length; i++) {
-        if (!visited[i] && k >= dungeons[i][0]) {
-            visited[i] = true;
-            k -= dungeons[i][1];
-            result.push(cnt + 1);
-            dfs(k, dungeons, visited, result, cnt + 1);
-            visited[i] = false;
-            k += dungeons[i][1];
+function solution(k, dungeons) {
+    const N = dungeons.length;
+    const visited = Array(N).fill(false);
+    let ans = 0;
+    
+    function dfs(k, cnt) {
+        ans = Math.max(ans, cnt);
+        
+        for (let i = 0; i < N; i++) {
+            if (!visited[i] && k >= dungeons[i][0]) {
+                visited[i] = true;
+                k -= dungeons[i][1];
+                
+                dfs(k, cnt + 1);
+                
+                visited[i] = false;
+                k += dungeons[i][1];
+            }
         }
     }
-}
-
-function solution(k, dungeons) {
-    const visited = Array(dungeons.length).fill(false);
-    const result = [];
-    let cnt = 0;
-    dfs(k, dungeons, visited, result, cnt);
-    return Math.max(...result);
+    
+    dfs(k, 0);
+    return ans;
 }
