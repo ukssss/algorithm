6b8ff4f7ee21bf474438c2d81b9cab2f35e6823e
ukss <1872003115su@gmail.com>
2024-01-29 18:03:49 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 0.27 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/README.md
+++ 프로그래머스/2/135807. 숫자 카드 나누기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 숫자 카드 나누기 - 135807 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/135807#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/135807) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.18 ms
+메모리: 33.5 MB, 시간: 0.27 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 1일 17:57:10
+2024년 1월 1일 18:3:48
 
 ### 문제 설명
 
--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.js
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.js
@@ -1,25 +1,14 @@
 function solution(arrayA, arrayB) {
     arrayA.sort((a, b) => a - b);
     arrayB.sort((a, b) => a - b);
-
-    let answer = 0;
-    let [gcdA, gcdB] = [arrayA[0], arrayB[0]];
-
-    for (let i = 1; i < arrayA.length; i++) {
-        gcdA = gcd(gcdA, arrayA[i]);
-        gcdB = gcd(gcdB, arrayB[i]);
-    }
-
-    if (gcdA === 1) gcdA = 0;
-    if (gcdB === 1) gcdB = 0;
-
-    if (arrayA.every((v) => v % gcdB !== 0)) answer = Math.max(answer, gcdB);
-    if (arrayB.every((v) => v % gcdA !== 0)) answer = Math.max(answer, gcdA);
-
-    return answer;
+    const checkA = find(arrayA, arrayB);
+    const checkB = find(arrayB, arrayA);
+    return Math.max(checkA, checkB);
 }
 
-function gcd(n1, n2) {
-    let rest = n1 % n2;
-    return n2 === 0 ? n1 : gcd(n2, rest);
+function find(curr, target) {
+    for (let i = curr[0]; i > 1; i--) {
+        if (curr.every((v) => v % i === 0) && target.every((v) => v % i !== 0)) return i;
+    }
+    return 0;
 }
