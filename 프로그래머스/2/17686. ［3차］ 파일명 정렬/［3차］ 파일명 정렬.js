16e0d49c9dca0902c5664e559e849d4690f78737
ukss <1872003115su@gmail.com>
2024-01-30 14:38:02 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 23.16 ms, Memory: 41.1 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -1,46 +1,20 @@
 function solution(files) {
-    const arr = [];
-
-    for (let i = 0; i < files.length; i++) {
-        for (let j = 0; j < files[i].length; j++) {
-            if (j === 0) {
-                arr.push({ HEAD: files[i][0], NUMBER: '', TAIL: '' });
-            } else if (
-                (files[i][j].charCodeAt() < 48 || files[i][j].charCodeAt() > 57) &&
-                arr[i]['NUMBER'] === ''
-            ) {
-                arr[i]['HEAD'] += files[i][j];
-            } else if (
-                files[i][j].charCodeAt() >= 48 &&
-                files[i][j].charCodeAt() <= 57 &&
-                arr[i]['TAIL'] === ''
-            ) {
-                arr[i]['NUMBER'] += files[i][j];
-            } else {
-                arr[i]['TAIL'] += files[i][j];
-            }
-        }
-    }
-
-    arr.sort((a, b) => {
-        let xh = a.HEAD.toLowerCase();
-        let yh = b.HEAD.toLowerCase();
-        let xn = Number(a.NUMBER);
-        let yn = Number(b.NUMBER);
-
-        if (xh < yh) {
-            return -1;
-        } else if (xh > yh) {
-            return 1;
-        } else {
-            if (xn < yn) {
-                return -1;
-            } else if (xn > yn) {
-                return 1;
-            }
-        }
-        return 0;
+    let wrap = files.map((file, idx) => ({ file, idx }));
+    wrap.sort((a, b) => {
+        const result = compare(a.file, b.file);
+        return result === 0 ? a.index - b.index : result;
     });
+    return wrap.map((v) => v.file);
+}
 
-    return arr.map((v) => v.HEAD + v.NUMBER + v.TAIL);
+function compare(a, b) {
+    const reg = /(\D*)([0-9]*)/i;
+    const A = a.match(reg);
+    const B = b.match(reg);
+    const compareHead = A[1].toLowerCase().localeCompare(B[1].toLowerCase());
+    return compareHead === 0 ? compareNumber(A[2], B[2]) : compareHead;
+}
+
+function compareNumber(a, b) {
+    return parseInt(a) > parseInt(b) ? 1 : parseInt(a) < parseInt(b) ? -1 : 0;
 }
