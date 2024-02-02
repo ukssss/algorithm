01c210c3430275c804ad1111a0040e393fd7042c
ukss <1872003115su@gmail.com>
2024-02-02 14:56:12 +0900

    [level 2] Title: 시소 짝꿍, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
@@ -0,0 +1,22 @@
+function solution(weights) {
+    let answer = 0;
+    weights.sort((a, b) => a - b);
+
+    const dict = {};
+
+    for (let weight of weights) {
+        if (dict[weight] === undefined) dict[weight] = 1;
+        else dict[weight] += 1;
+    }
+
+    for (let weight of weights) {
+        if (dict[weight] > 1) answer += dict[weight] - 1;
+        if (dict[weight * (3 / 2)] > 0) answer += dict[weight * (3 / 2)];
+        if (dict[weight * 2] > 0) answer += dict[weight * 2];
+        if (dict[weight * (4 / 3)] > 0) answer += dict[weight * (4 / 3)];
+        dict[weight] -= 1;
+    }
+
+    return answer;
+
+}
